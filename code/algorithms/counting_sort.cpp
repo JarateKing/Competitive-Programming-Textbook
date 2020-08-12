bed03eabcdc670acd6445c7906cd6ecd170d4435
JarateKing <scmacintosh@upei.ca>
2020-08-12 00:21:18 -0300

    added concepts about counting sort

--- code/algorithms/counting_sort.cpp
+++ code/algorithms/counting_sort.cpp
@@ -1,8 +1,8 @@
-// generate the counts array
+// generate the counts array from `arr`
 for (int i = 0; i < n; i++)
-	counts[unsorted[i]]++;
+	counts[arr[i]]++;
 
-// generate the array from the counts
+// sort `arr` using the counts array
 int p = 0;
 for (int i = 0; i < 10; i++) {
 	for (int j = 0; j < counts[i]; j++) {
