5ed1554aceb5028beecc011e7ada28d54eb65764
JarateKing <scmacintosh@upei.ca>
2020-08-26 14:04:29 -0300

    fixed java code using spaces

--- problems/hello_world/solution.java
+++ problems/hello_world/solution.java
@@ -1,5 +1,5 @@
 class Driver {
-    public static void main(String[] args) {
-        System.out.println("Hello World!"); 
-    }
+	public static void main(String[] args) {
+		System.out.println("Hello World!"); 
+	}
 }
