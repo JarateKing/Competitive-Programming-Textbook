3f6fda5fb6f86ac1c279a949982bee4a4bae0ff0
JarateKing <scmacintosh@upei.ca>
2020-09-08 16:50:37 -0300

    added comments to string methods

--- code/string/is_anagram_1.cpp
+++ code/string/is_anagram_1.cpp
@@ -1,5 +1,9 @@
 bool isAnagram(string a, string b) {
+	// make sure both strings are ordered alphabetically
 	sort(a.begin(), a.end());
 	sort(b.begin(), b.end());
+	
+	// check if the two strings are the same
+	// in other words, they have the same amount of each character
 	return a == b;
 }
--- code/string/is_palindrome.cpp
+++ code/string/is_palindrome.cpp
@@ -1,5 +1,8 @@
 bool isPalindrome(string str) {
 	int n = str.length();
+	
+	// for every character in the first half of the string
+	// make sure it's the same as its inverse
 	for (int i = 0; i < n/2; i++)
 		if (str[i] != str[n-1-i])
 			return false;
